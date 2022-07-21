37b2e0f508148bfe8c7689c9324dbb52581c6a68
Brian Wang <brrian@gmail.com>
2022-07-21 15:41:29 -0500

    feat: add initial styling and a11y for select

--- packages/main/src/helpers/getThemeCss.ts
+++ packages/main/src/helpers/getThemeCss.ts
@@ -2,18 +2,20 @@ import { ThemeConfig } from '@domain';
 import { nativeTheme } from 'electron';
 
 const colors = {
-  dark: 'rgb(35,35,35)',
-  darkAlt: 'rgb(56,56,56)',
-  lightAlt: 'rgb(240,240,240)',
-  longBreak: 'rgb(75,174,79)',
-  primary: 'rgb(0,194,193)',
-  shortBreak: 'rgb(32,149,242)',
-  task: 'rgb(243,66,53)',
-  white: 'rgb(255,255,255)',
+  black: 'rgb(0 0 0)',
+  blue: 'rgb(67 134 250)',
+  dark: 'rgb(35 35 35)',
+  darkAlt: 'rgb(56 56 56)',
+  lightAlt: 'rgb(240 240 240)',
+  longBreak: 'rgb(75 174 79)',
+  primary: 'rgb(0 194 193)',
+  shortBreak: 'rgb(32 149 242)',
+  task: 'rgb(243 66 53)',
+  white: 'rgb(255 255 255)',
 };
 
 const opacity = (color: string, opacity: number): string => {
-  return `rgba${color.slice(3, -1)},${opacity}%)`;
+  return `rgba${color.slice(3, -1)} / ${opacity}%)`;
 };
 
 const defaultTheme: ThemeConfig = {
@@ -25,7 +27,7 @@ const defaultTheme: ThemeConfig = {
       'long-break': [colors.longBreak, colors.dark],
     },
     heading: {
-      default: `${opacity(colors.white, 60)}`,
+      default: opacity(colors.white, 60),
       task: colors.white,
       'short-break': colors.white,
       'long-break': colors.white,
@@ -41,24 +43,30 @@ const defaultTheme: ThemeConfig = {
     toggle: {
       background: {
         default: 'transparent',
-        hover: [`${opacity(colors.dark, 4)}`, `${opacity(colors.white, 2)}`],
+        hover: [opacity(colors.dark, 4), opacity(colors.white, 2)],
       },
       content: {
-        default: `${opacity(colors.white, 38)}`,
-        hover: `${opacity(colors.white, 60)}`,
+        default: opacity(colors.white, 38),
+        hover: opacity(colors.white, 60),
       },
     },
     button: {
       background: {
         default: 'transparent',
-        hover: [`${opacity(colors.dark, 8)}`, `${opacity(colors.dark, 60)}`],
+        hover: [opacity(colors.dark, 8), opacity(colors.dark, 60)],
       },
       content: {
-        default: [`${opacity(colors.dark, 87)}`, colors.white],
-        hover: [`${opacity(colors.dark, 87)}`, colors.white],
+        default: [opacity(colors.dark, 87), colors.white],
+        hover: [opacity(colors.dark, 87), colors.white],
       },
     },
     background: [colors.lightAlt, colors.darkAlt],
+  },
+  select: {
+    background: [colors.white, colors.darkAlt],
+    content: [opacity(colors.black, 87), colors.white],
+    group: [opacity(colors.black, 4), opacity(colors.black, 20)],
+    selected: [opacity(colors.blue, 20), opacity(colors.white, 10)],
   },
 };
 
--- packages/renderer/src/select/components/DropdownItem/DropdownItem.tsx
+++ packages/renderer/src/select/components/DropdownItem/DropdownItem.tsx
@@ -1,37 +1,49 @@
 import { SelectItem, SelectOptionType, Service } from '@domain';
+import cc from 'classcat';
 import { FC } from 'react';
 import DropdownList from '../DropdownList';
 import DropdownRow from '../DropdownRow';
-import Option from '../Option';
-import OptionGroup from '../OptionGroup';
+import styles from './DropdownItem.module.scss';
 
 interface DropdownItemProps {
   depth: number;
   item: SelectItem;
+  onOptionHover(option: SelectOptionType): void;
   onOptionSelect(option: SelectOptionType): void;
+  selectedOption?: SelectOptionType;
   service?: Service;
 }
 
-const DropdownItem: FC<DropdownItemProps> = ({ depth, item, onOptionSelect, service }) => {
+const DropdownItem: FC<DropdownItemProps> = ({
+  depth,
+  item,
+  onOptionHover,
+  onOptionSelect,
+  selectedOption,
+  service,
+}) => {
   const isGroupType = item.type === 'group' || item.type === 'customGroup';
 
   if (isGroupType) {
     return (
-      <>
-        <DropdownRow depth={depth + 1}>
+      <DropdownList
+        aria-labelledby={item.id}
+        depth={depth + 1}
+        items={item.items}
+        onOptionHover={onOptionHover}
+        onOptionSelect={onOptionSelect}
+        selectedOption={selectedOption}
+        role="group"
+        service={service}
+      >
+        <DropdownRow className={styles.group} id={item.id} role="presentation">
           {item.type === 'customGroup' && service?.CustomSelectGroup ? (
             <service.CustomSelectGroup group={item} />
           ) : (
-            <OptionGroup group={item} />
+            item.label
           )}
         </DropdownRow>
-        <DropdownList
-          depth={depth + 1}
-          items={item.items}
-          onOptionSelect={onOptionSelect}
-          service={service}
-        />
-      </>
+      </DropdownList>
     );
   }
 
@@ -39,12 +51,23 @@ const DropdownItem: FC<DropdownItemProps> = ({ depth, item, onOptionSelect, serv
     onOptionSelect(item);
   };
 
+  const handleOptionMouseOver = () => {
+    onOptionHover(item);
+  };
+
   return (
-    <DropdownRow depth={depth} onClick={handleOptionClick}>
+    <DropdownRow
+      className={cc({
+        [styles.selected]: selectedOption === item,
+      })}
+      onClick={handleOptionClick}
+      onMouseOver={handleOptionMouseOver}
+      role="option"
+    >
       {item.type === 'customOption' && service?.CustomSelectOption ? (
         <service.CustomSelectOption option={item} children={item.label} />
       ) : (
-        <Option option={item} children={item.label} />
+        item.label
       )}
     </DropdownRow>
   );
