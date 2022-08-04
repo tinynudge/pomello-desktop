36e50defa599c53c52c7a82f99c45f88966db0cf
Brian Wang <brrian@gmail.com>
2022-08-04 14:16:21 -0500

    refactor: clean up select task view

--- packages/renderer/src/app/views/SelectTaskView/SelectTaskView.tsx
+++ packages/renderer/src/app/views/SelectTaskView/SelectTaskView.tsx
@@ -1,33 +1,32 @@
 import usePomelloActions from '@/app/hooks/usePomelloActions';
 import SelectField from '@/app/ui/SelectField';
 import useTranslation from '@/shared/hooks/useTranslation';
-import { Service } from '@domain';
+import { SelectItem } from '@domain';
 import { FC } from 'react';
 import { useQuery } from 'react-query';
 
 interface SelectTaskViewProps {
-  service: Service;
+  fetchTasks(): Promise<SelectItem[]>;
+  serviceId: string;
 }
 
-const SelectTaskView: FC<SelectTaskViewProps> = ({ service }) => {
+const SelectTaskView: FC<SelectTaskViewProps> = ({ fetchTasks, serviceId }) => {
   const { t } = useTranslation();
 
   const { selectTask } = usePomelloActions();
 
-  const { data: tasks } = useQuery(`${service.id}-tasks`, service.fetchTasks);
+  const { data: tasks } = useQuery(`${serviceId}-tasks`, fetchTasks);
 
   const handleTaskSelect = (id: string) => {
     selectTask(id);
   };
 
   return (
-    <>
-      <SelectField
-        items={tasks!}
-        onChange={handleTaskSelect}
-        placeholder={t('selectTaskPlaceholder')}
-      />
-    </>
+    <SelectField
+      items={tasks!}
+      onChange={handleTaskSelect}
+      placeholder={t('selectTaskPlaceholder')}
+    />
   );
 };
 
