be399a7c54529fd722cc554800834fd7ff4f7bd8
Brian Wang <brrian@gmail.com>
2022-12-10 21:15:19 -0600

    chore: upgrade vite and vitest

--- packages/renderer/vite.config.js
+++ packages/renderer/vite.config.js
@@ -1,4 +1,4 @@
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { builtinModules } from 'module';
 import { join } from 'path';
 import { defineConfig } from 'vite';
