1952f7584cd2163eb825c5dae0c3e28df2515fb8
Brian Wang <brrian@gmail.com>
2023-03-18 18:59:04 -0500

    chore: upgrade to TypeScript 5

--- packages/domain/StoreContents.ts
+++ packages/domain/StoreContents.ts
@@ -1,1 +1,2 @@
-export type StoreContents = Record<string, unknown>;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type StoreContents = Record<string, any>;
--- packages/domain/StoreManager.ts
+++ packages/domain/StoreManager.ts
@@ -3,8 +3,8 @@ import { Store } from './Store';
 import { StoreContents } from './StoreContents';
 
 export interface StoreManager {
-  getOrFailStore<TContents = StoreContents>(id: string): Store<TContents>;
-  registerStore<TContents = StoreContents>(
+  getOrFailStore<TContents extends StoreContents = StoreContents>(id: string): Store<TContents>;
+  registerStore<TContents extends StoreContents = StoreContents>(
     options: RegisterStoreOptions<TContents>
   ): Store<TContents>;
 }
