a69100ed9dfb100b655ead88376bff5ef6d58372
Brian Wang <brrian@gmail.com>
2023-05-03 20:09:58 -0500

    feat(trello): optimistically remove Trello cards

--- packages/renderer/src/services/trello/getTaskCompleteItems.ts
+++ packages/renderer/src/services/trello/getTaskCompleteItems.ts
@@ -18,6 +18,10 @@ const getTaskCompleteItems = (
   }
 
   completeCheckItem(runtime, task);
+
+  return {
+    shouldRemoveTaskFromCache: true,
+  };
 };
 
 export default getTaskCompleteItems;
--- packages/renderer/src/services/trello/onTaskCompletePromptHandled.ts
+++ packages/renderer/src/services/trello/onTaskCompletePromptHandled.ts
@@ -1,4 +1,4 @@
-import { TaskCompletePromptHandledEvent } from '@domain';
+import { TaskCompletePromptHandledEvent, TaskCompletePromptHandledResponse } from '@domain';
 import { TrelloRuntime } from './TrelloRuntime';
 import findOrFailTask from './helpers/findOrFailTask';
 import isCheckItem from './helpers/isCheckItem';
@@ -7,12 +7,17 @@ import moveCardAndUpdateDoneList from './helpers/moveCardAndUpdateDoneList';
 const onTaskCompletePromptHandled = (
   runtime: TrelloRuntime,
   { optionId, taskId }: TaskCompletePromptHandledEvent
-): void => {
+): TaskCompletePromptHandledResponse => {
   const task = findOrFailTask(runtime.cache, taskId);
+  const isCurrentList = runtime.cache.get().currentList.id === optionId;
 
-  if (!isCheckItem(task)) {
+  if (!isCheckItem(task) && !isCurrentList) {
     moveCardAndUpdateDoneList(runtime, task, optionId);
   }
+
+  return {
+    shouldRemoveTaskFromCache: !isCurrentList,
+  };
 };
 
 export default onTaskCompletePromptHandled;
