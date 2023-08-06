5c4c2a54df12d8600c6aade2a29627f6bc570a12
Brian Wang <brrian@gmail.com>
2023-08-05 22:13:49 -0500

    tests: add select tests

---
+++ packages/renderer/src/select/_fixtures__/MockCustomSelectGroup.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { CustomSelectGroupProps } from '@domain';
+
+  type $$Props = CustomSelectGroupProps;
+
+  export let group: $$Props['group'];
+</script>
+
+Custom Group - {group.label}
