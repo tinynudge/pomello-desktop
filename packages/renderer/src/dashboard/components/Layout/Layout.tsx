f09625df7dd252e92525bbb95c54f286aad7a66b
Brian Wang <brrian@gmail.com>
2024-07-20 12:55:03 -0500

    feat: add dashboard account details

--- packages/renderer/src/dashboard/components/Layout/Layout.tsx
+++ packages/renderer/src/dashboard/components/Layout/Layout.tsx
@@ -1,11 +1,20 @@
-import { useTranslate } from '@/shared/context/RuntimeContext';
+import { usePomelloConfig, useTranslate } from '@/shared/context/RuntimeContext';
+import { Translate } from '@/ui/shared/Translate';
 import { DashboardRoute } from '@pomello-desktop/domain';
 import { A } from '@solidjs/router';
-import { For, ParentComponent } from 'solid-js';
+import { For, ParentComponent, Show } from 'solid-js';
 import styles from './Layout.module.scss';
 
 export const Layout: ParentComponent = props => {
+  const { store } = usePomelloConfig();
   const t = useTranslate();
+
+  const handleAuthButtonClick = (action: 'authorize' | 'register') => {
+    window.app.showAuthWindow({
+      type: 'pomello',
+      action,
+    });
+  };
 
   const routes: [DashboardRoute, label: string][] = [
     [DashboardRoute.Productivity, t('dashboardMenuProductivity')],
@@ -18,8 +27,8 @@ export const Layout: ParentComponent = props => {
   return (
     <div class={styles.layout}>
       <div class={styles.sidebar}>
-        <nav aria-label={t('dashboardMenuLabel')}>
-          <ul class={styles.menu}>
+        <nav aria-label={t('dashboardMenuLabel')} class={styles.menu}>
+          <ul>
             <For each={routes}>
               {([route, label]) => (
                 <li>
@@ -31,6 +40,41 @@ export const Layout: ParentComponent = props => {
             </For>
           </ul>
         </nav>
+        <div class={styles.accountDetails} data-testid="account-details">
+          <Show
+            fallback={
+              <Translate
+                components={{
+                  logIn: props => (
+                    <button
+                      class={styles.button}
+                      onClick={() => handleAuthButtonClick('authorize')}
+                    >
+                      {props.children}
+                    </button>
+                  ),
+                  signUp: props => (
+                    <button class={styles.button} onClick={() => handleAuthButtonClick('register')}>
+                      {props.children}
+                    </button>
+                  ),
+                }}
+                key="trackProductivityMessage"
+              />
+            }
+            when={store.user}
+          >
+            {getUser => (
+              <>
+                <span class={styles.badge} data-type={getUser().type}>
+                  {t(`${getUser().type}AccountLabel`)}
+                </span>
+                <p>{getUser().name}</p>
+                <p>{getUser().email}</p>
+              </>
+            )}
+          </Show>
+        </div>
       </div>
       <section class={styles.content}>{props.children}</section>
     </div>
