14215fc143490eb249c5b883aaa59a2a0fd62320
Brian Wang <brrian@gmail.com>
2024-07-29 22:09:13 -0500

    fix: fix prop spreading in Panel component

--- packages/renderer/src/ui/dashboard/Panel/Panel.spec.tsx
+++ packages/renderer/src/ui/dashboard/Panel/Panel.spec.tsx
@@ -13,16 +13,16 @@ describe('UI - Panel', () => {
   it('should render the panel list', async () => {
     renderComponent(() => (
       <Panel heading="H.S.K.T.">
-        <Panel.List>
+        <Panel.List aria-label="H.S.K.T.">
           <Panel.List.Item>Head</Panel.List.Item>
-          <Panel.List.Item>Shoulders</Panel.List.Item>
+          <Panel.List.Item aria-hidden>Shoulders</Panel.List.Item>
           <Panel.List.Item>Knees</Panel.List.Item>
           <Panel.List.Item>Toes</Panel.List.Item>
         </Panel.List>
       </Panel>
     ));
 
-    expect(screen.getByRole('list')).toBeInTheDocument();
-    expect(screen.getAllByRole('listitem')).toHaveLength(4);
+    expect(screen.getByRole('list', { name: 'H.S.K.T.' })).toBeInTheDocument();
+    expect(screen.getAllByRole('listitem')).toHaveLength(3);
   });
 });
