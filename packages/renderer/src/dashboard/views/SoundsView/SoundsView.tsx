a80c075d7467ea8b72cda592bcd8bd095117119a
Brian Wang <brrian@gmail.com>
2024-09-01 15:36:13 -0500

    feat: add timer sounds dropdown

--- packages/renderer/src/dashboard/views/SoundsView/SoundsView.tsx
+++ packages/renderer/src/dashboard/views/SoundsView/SoundsView.tsx
@@ -1,27 +1,62 @@
 import { MainHeader } from '@/dashboard/components/MainHeader';
+import { useDashboard } from '@/dashboard/context/DashboardContext';
 import { useTranslate } from '@/shared/context/RuntimeContext';
 import { Panel } from '@/ui/dashboard/Panel';
+import { Select } from '@/ui/dashboard/Select';
 import { Component, For } from 'solid-js';
+
+type TimerType = (typeof timerTypes)[number];
+
+type TimerPhase = (typeof timerPhases)[number];
 
 const timerTypes = ['task', 'shortBreak', 'longBreak'] as const;
 
-const timerPhases = ['start', 'tick', 'end'] as const;
+const timerPhases = ['Start', 'Tick', 'End'] as const;
 
 export const SoundsView: Component = () => {
+  const { getSetting, stageSetting } = useDashboard();
   const t = useTranslate();
+
+  const handleSoundChange = (type: TimerType, phase: TimerPhase, sound: string) => {
+    stageSetting(`${type}Timer${phase}Sound`, sound);
+  };
+
+  const defaultSoundOptions = [
+    {
+      id: 'wind-up',
+      label: t('sound.windUp'),
+    },
+    {
+      id: 'egg-timer',
+      label: t('sound.eggTimer'),
+    },
+    {
+      id: 'ding',
+      label: t('sound.ding'),
+    },
+  ];
+
   return (
     <>
       <MainHeader heading={t('routeSounds')} />
       <For each={timerTypes}>
-        {timerType => (
-          <Panel heading={t(`${timerType}SoundsHeader`)} isPaddingDisabled>
-            <Panel.List aria-label={t(`${timerType}SoundsLabel`)}>
+        {type => (
+          <Panel heading={t(`${type}SoundsHeader`)} isPaddingDisabled>
+            <Panel.List aria-label={t(`${type}SoundsLabel`)}>
               <For each={timerPhases}>
                 {phase => (
                   <Panel.List.FormField
-                    for={`${timerType}-${phase}`}
-                    label={t(`sounds.${phase}`)}
-                  />
+                    for={`${type}-${phase}`}
+                    label={t(`sounds.${phase.toLowerCase()}`)}
+                  >
+                    <Select
+                      aria-label={t(`sounds.${type}${phase}`)}
+                      id={`${type}${phase}`}
+                      onChange={sound => handleSoundChange(type, phase, sound)}
+                      options={defaultSoundOptions}
+                      value={getSetting(`${type}Timer${phase}Sound`) ?? undefined}
+                    />
+                  </Panel.List.FormField>
                 )}
               </For>
             </Panel.List>
