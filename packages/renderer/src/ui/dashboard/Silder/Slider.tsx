7d603781516fc26cca96dec9934d247e02a982d5
Brian Wang <brrian@gmail.com>
2024-09-02 14:44:20 -0500

    feat: create Slider component

---
+++ packages/renderer/src/ui/dashboard/Silder/Slider.tsx
@@ -0,0 +1,45 @@
+import cc from 'classcat';
+import { Component, JSX, onCleanup, onMount, splitProps } from 'solid-js';
+import styles from './Slider.module.scss';
+
+type SliderProps = Omit<JSX.InputHTMLAttributes<HTMLInputElement>, 'type'>;
+
+export const Slider: Component<SliderProps> = allProps => {
+  const [props, remainingProps] = splitProps(allProps, ['class', 'ref']);
+
+  onMount(() => {
+    updateProgress();
+
+    inputRef.addEventListener('input', updateProgress);
+
+    onCleanup(() => {
+      inputRef.removeEventListener('input', updateProgress);
+    });
+  });
+
+  const attachRef = (element: HTMLInputElement) => {
+    inputRef = element;
+
+    /* @ts-expect-error Solid uses the callback form when forwarding refs */
+    props.ref?.(element);
+  };
+
+  const updateProgress = () => {
+    const max = inputRef.max ? +inputRef.max : 100;
+    const min = inputRef.min ? +inputRef.min : 0;
+    const progress = (+inputRef.value / (max - min)) * 100;
+
+    inputRef.style.setProperty('--slider-progress', `${progress}%`);
+  };
+
+  let inputRef: HTMLInputElement;
+
+  return (
+    <input
+      {...remainingProps}
+      class={cc([styles.slider, props.class])}
+      ref={element => attachRef(element)}
+      type="range"
+    />
+  );
+};
