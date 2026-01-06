c7a90a5797fcddeec10fc379f6c9616461ba228d
Brian Wang <brrian@gmail.com>
2026-01-05 18:22:19 -0600

    feat: include ServiceContainer to Configure view

--- packages/renderer/src/dashboard/views/ServicesView/ServiceView.tsx
+++ packages/renderer/src/dashboard/views/ServicesView/ServiceView.tsx
@@ -1,4 +1,5 @@
 import { MainHeader } from '@/dashboard/components/MainHeader';
+import { ServiceContainer } from '@/shared/components/ServiceContainer';
 import { ConfigureServiceConfigProvider } from '@/shared/context/ConfigureServiceConfigContext';
 import { useServiceResource } from '@/shared/context/ServiceContext';
 import { ServiceConfig, StoreContents } from '@pomello-desktop/domain';
@@ -14,10 +15,12 @@ export const ServiceView: Component = () => {
         <ConfigureServiceConfigProvider
           initialServiceConfig={getServiceResource().config as ServiceConfig<StoreContents> | null}
         >
-          <MainHeader heading={getServiceResource().service.displayName} />
-          <Show when={getServiceResource().service.ConfigureView}>
-            {getConfigureView => <Dynamic component={getConfigureView()} />}
-          </Show>
+          <ServiceContainer>
+            <MainHeader heading={getServiceResource().service.displayName} />
+            <Show when={getServiceResource().service.ConfigureView}>
+              {getConfigureView => <Dynamic component={getConfigureView()} />}
+            </Show>
+          </ServiceContainer>
         </ConfigureServiceConfigProvider>
       )}
     </Show>
