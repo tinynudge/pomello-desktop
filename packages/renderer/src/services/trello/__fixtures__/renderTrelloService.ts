3c47a10aa1b00a28a0b480eb9ad7300ddc22899b
Brian Wang <brrian@gmail.com>
2026-01-10 20:58:11 -0600

    refactor: centralize mock Trello API creation

--- packages/renderer/src/services/trello/__fixtures__/renderTrelloService.ts
+++ packages/renderer/src/services/trello/__fixtures__/renderTrelloService.ts
@@ -1,50 +1,11 @@
-import { createHttpResponse } from '@/__fixtures__/createHttpResponse';
 import { createMockServiceConfig } from '@/__fixtures__/createMockServiceConfig';
-import { mockServer } from '@/__fixtures__/mockServer';
 import { renderApp, RenderAppOptions } from '@/app/__fixtures__/renderApp';
-import { DefaultBodyType, http, HttpResponseResolver, PathParams } from 'msw';
 import { createTrelloService } from '..';
-import { TRELLO_API_URL } from '../constants';
-import {
-  TrelloCache,
-  TrelloCard,
-  TrelloCardAction,
-  TrelloCheckItem,
-  TrelloConfigStore,
-  TrelloLabel,
-  TrelloMember,
-} from '../domain';
+import { TrelloCache, TrelloConfigStore } from '../domain';
 import translations from '../translations/en-US.json';
-import { generateTrelloCard } from './generateTrelloCard';
-import { generateTrelloCardAction } from './generateTrelloCardAction';
-import { generateTrelloCheckItem } from './generateTrelloCheckItem';
-import { generateTrelloLabel } from './generateTrelloLabel';
-import { generateTrelloMember } from './generateTrelloMember';
+import { mockTrelloApi, TrelloApiResponses } from './mockTrelloApi';
 
 export * from '@solidjs/testing-library';
-
-type TrelloApiResponses = {
-  addComment:
-    | TrelloCardAction
-    | HttpResponseResolver<PathParams, DefaultBodyType, TrelloCardAction>;
-  createCard: TrelloCard | HttpResponseResolver<PathParams, DefaultBodyType, TrelloCard>;
-  fetchBoardsAndLists:
-    | TrelloMember
-    | HttpResponseResolver<PathParams, DefaultBodyType, TrelloMember>;
-  fetchCardsByListId:
-    | TrelloCard[]
-    | HttpResponseResolver<PathParams, DefaultBodyType, TrelloCard[]>;
-  fetchLabelsByBoardId:
-    | TrelloLabel[]
-    | HttpResponseResolver<PathParams, DefaultBodyType, TrelloLabel[]>;
-  markCheckItemComplete:
-    | TrelloCheckItem
-    | HttpResponseResolver<PathParams, DefaultBodyType, TrelloCheckItem>;
-  moveCardToList: TrelloCard | HttpResponseResolver<PathParams, DefaultBodyType, TrelloCard>;
-  updateComment:
-    | TrelloCardAction
-    | HttpResponseResolver<PathParams, DefaultBodyType, TrelloCardAction>;
-};
 
 type RenderTrelloServiceOptions = Pick<RenderAppOptions, 'appApi' | 'pomelloApi' | 'settings'> & {
   config?: Partial<TrelloConfigStore>;
@@ -71,43 +32,7 @@ export const renderTrelloService = async ({
 }: RenderTrelloServiceOptions = {}) => {
   cache = (await originalCreateTrelloCache)();
 
-  mockServer.use(
-    http.get(
-      `${TRELLO_API_URL}members/me`,
-      createHttpResponse<TrelloMember>(generateTrelloMember(), trelloApi?.fetchBoardsAndLists)
-    ),
-    http.get(
-      `${TRELLO_API_URL}boards/:boardId/labels`,
-      createHttpResponse<TrelloLabel[]>([generateTrelloLabel()], trelloApi?.fetchLabelsByBoardId)
-    ),
-    http.get(
-      `${TRELLO_API_URL}lists/:listId/cards`,
-      createHttpResponse<TrelloCard[]>([generateTrelloCard()], trelloApi?.fetchCardsByListId)
-    ),
-    http.post(
-      `${TRELLO_API_URL}cards`,
-      createHttpResponse<TrelloCard>(generateTrelloCard(), trelloApi?.createCard)
-    ),
-    http.post(
-      `${TRELLO_API_URL}cards/:idCard/actions/comments`,
-      createHttpResponse<TrelloCardAction>(generateTrelloCardAction(), trelloApi?.addComment)
-    ),
-    http.put(
-      `${TRELLO_API_URL}cards/:idCard/checkItem/:idCheckItem`,
-      createHttpResponse<TrelloCheckItem>(
-        generateTrelloCheckItem(),
-        trelloApi?.markCheckItemComplete
-      )
-    ),
-    http.put(
-      `${TRELLO_API_URL}cards/:idCard`,
-      createHttpResponse<TrelloCard>(generateTrelloCard(), trelloApi?.moveCardToList)
-    ),
-    http.put(
-      `${TRELLO_API_URL}actions/:idAction`,
-      createHttpResponse<TrelloCardAction>(generateTrelloCardAction(), trelloApi?.updateComment)
-    )
-  );
+  mockTrelloApi(trelloApi);
 
   const [config, configActions] = createMockServiceConfig(createTrelloService.id, {
     token: 'MY_TRELLO_TOKEN',
