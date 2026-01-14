7d99e333bf4ff7bc9190c3145df2d55b51de53b7
Brian Wang <brrian@gmail.com>
2026-01-13 19:41:55 -0600

    fix: restore zip artifacts for Mac builds

--- .electron-builder.config.cjs
+++ .electron-builder.config.cjs
@@ -24,7 +24,7 @@ const config = {
     hardenedRuntime: true,
     target: {
       arch: 'universal',
-      target: 'dmg',
+      target: 'default',
     },
   },
   generateUpdatesFilesForAllChannels: true,
