c63f52ce3de501e7e750e54d1a55371ff4fd3366
Brian Wang <brrian@gmail.com>
2026-01-31 07:43:37 -0600

    feat: support complete task option in task complete

--- packages/domain/src/TaskCompleteItems.ts
+++ packages/domain/src/TaskCompleteItems.ts
@@ -2,6 +2,7 @@ import { RemoveTask } from './RemoveTask';
 import { SelectItem } from './SelectItem';
 
 export type TaskCompleteItems = {
+  completeTaskId?: string;
   items?: SelectItem[];
   moveTaskItemId?: string;
   removeTask?: RemoveTask;
