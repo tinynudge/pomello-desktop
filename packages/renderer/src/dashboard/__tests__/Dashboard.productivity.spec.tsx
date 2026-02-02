8ec5c5e772184e0dfbd9bda0f369a791b6da404b
Brian Wang <brrian@gmail.com>
2026-02-01 20:50:53 -0600

    feat(dashboard): authenticate productivity view

--- packages/renderer/src/dashboard/__tests__/Dashboard.productivity.spec.tsx
+++ packages/renderer/src/dashboard/__tests__/Dashboard.productivity.spec.tsx
@@ -33,4 +33,82 @@ describe('Dashboard - Productivity', () => {
 
     vi.useRealTimers();
   });
+
+  it('should show a login view when not logged in', () => {
+    renderDashboard({
+      route: DashboardRoute.Productivity,
+      pomelloConfig: {
+        user: undefined,
+      },
+    });
+
+    const loginView = screen.getByTestId('login-view');
+
+    expect(screen.queryByRole('heading', { name: 'Productivity' })).not.toBeInTheDocument();
+    expect(
+      within(loginView).getByText('You must be logged in to Pomello to view this.')
+    ).toBeInTheDocument();
+    expect(within(loginView).getByRole('button', { name: 'Log in' })).toBeInTheDocument();
+    expect(
+      within(loginView).getByText(
+        (_content, element) => element?.textContent === 'No account? Sign up for free.'
+      )
+    ).toBeInTheDocument();
+  });
+
+  it('should open the login page when clicking the log in button', async () => {
+    const { appApi, userEvent } = renderDashboard({
+      route: DashboardRoute.Productivity,
+      pomelloConfig: {
+        user: undefined,
+      },
+    });
+
+    const loginView = screen.getByTestId('login-view');
+
+    await userEvent.click(within(loginView).getByRole('button', { name: 'Log in' }));
+
+    expect(appApi.showAuthWindow).toHaveBeenCalledWith({
+      type: 'pomello',
+      action: 'authorize',
+    });
+  });
+
+  it('should open the sign up page when clicking the sign up button', async () => {
+    const { appApi, userEvent } = renderDashboard({
+      route: DashboardRoute.Productivity,
+      pomelloConfig: {
+        user: undefined,
+      },
+    });
+
+    const loginView = screen.getByTestId('login-view');
+
+    await userEvent.click(within(loginView).getByRole('button', { name: 'Sign up' }));
+
+    expect(appApi.showAuthWindow).toHaveBeenCalledWith({
+      type: 'pomello',
+      action: 'register',
+    });
+  });
+
+  it('should show the productivity view when logging in', async () => {
+    const { pomelloConfig } = renderDashboard({
+      route: DashboardRoute.Productivity,
+      pomelloConfig: {
+        user: undefined,
+      },
+    });
+
+    expect(screen.queryByRole('heading', { name: 'Productivity' })).not.toBeInTheDocument();
+
+    pomelloConfig.set('user', {
+      email: 'thomas@tester.com',
+      name: 'Thomas Tester',
+      timezone: 'America/New_York',
+      type: 'premium',
+    });
+
+    expect(screen.getByRole('heading', { name: 'Productivity', level: 1 })).toBeInTheDocument();
+  });
 });
--- packages/renderer/src/dashboard/views/ProductivityView/ProductivityView.tsx
+++ packages/renderer/src/dashboard/views/ProductivityView/ProductivityView.tsx
@@ -1,21 +1,23 @@
 import { MainHeader } from '@/dashboard/components/MainHeader';
-import { useTranslate } from '@/shared/context/RuntimeContext';
+import { usePomelloConfig, useTranslate } from '@/shared/context/RuntimeContext';
 import { Button } from '@/ui/dashboard/Button';
 import { Panel } from '@/ui/dashboard/Panel';
 import { format, startOfWeek } from 'date-fns';
-import { Component } from 'solid-js';
+import { Component, Show } from 'solid-js';
+import { LoginView } from './LoginView';
 
 export const ProductivityView: Component = () => {
+  const pomelloConfig = usePomelloConfig();
   const t = useTranslate();
 
   return (
-    <>
+    <Show when={pomelloConfig.store.user} fallback={<LoginView />}>
       <MainHeader heading={t('routeProductivity')}>
         <Button>Export data</Button>
       </MainHeader>
       <Panel heading={t('todayLabel')} />
       <Panel heading={t('thisWeekLabel', { week: format(startOfWeek(new Date()), 'MMMM d') })} />
       <Panel heading={t('productivityHistoryLabel')} />
-    </>
+    </Show>
   );
 };
